1b5cb60307f4efd1209cac5fff868fc4afea0db2
Christian Bueno <me@christianbueno.com>
2013-04-10 03:14:11 -0300

    Colocação dos Formulários InfusionSoft
    -Incluídos formulários em suas páginas respectivas
    -Ajustadas devidas páginas, com fundos diferentes
    -CSS simplificado para cada formulário
    
    Signed-off-by: Christian Bueno <me@christianbueno.com>

--- fuel/app/views/web/forms/artistas.php
+++ fuel/app/views/web/forms/artistas.php
@@ -7,242 +7,17 @@
     <?php echo Asset::css('main.css'); ?>
 
 </head>
-<body class="zulub">
+<body class="artistas">
     <div id="vignette" class="container-fluid">     
 
-        <span id="logo" class="offset2"></span>
+        <span id="logo" class="offset2 branco"></span>
 
         <div class="texto-wrapper">
-        <h1>Fale conosco</h1>
-			<style type="text/css">
-			.beta-base .preheader, .beta-base .header, .beta-base .sidebar, .beta-base .body, .beta-base .footer, #mainContent {
-			text-align: left;
-			}
-			.beta-base .preheader, .beta-base .header, .beta-base .body, .beta-base .sidebar, .beta-base .leftSidebar, .beta-base .rightSidebar, .beta-base .footer {
-			margin: 0;
-			padding: 0;
-			border: none;
-			white-space: normal;
-			line-height: normal;
-			}
-			.beta-base .title, .beta-base .subtitle, .beta-base .text, .beta-base img {
-			margin: 0;
-			padding: 0;
-			background: none;
-			border: none;
-			white-space: normal;
-			line-height: normal;
-			}
-			.beta-base .bodyContainer td.preheader{
-			padding: 10px 0;
-			}
-			.beta-base .bodyContainer td.header {
-			padding: 0;
-			height: 30px;
-			}
-			.beta-base .bodyContainer td.body, .beta-base .bodyContainer td.footer,
-			.beta-base .bodyContainer td.sidebar, .beta-base .bodyContainer td.leftSidebar, .beta-base .bodyContainer td.rightSidebar {
-			padding: 20px;
-			}
-			.beta-base .bodyContainer td.header p, .beta-base .bodyContainer td.preheader p, .beta-base .bodyContainer td.body p,
-			.beta-base .bodyContainer td.footer p, .beta-base .bodyContainer td.sidebar p,
-			.beta-base .bodyContainer td.leftSidebar p, .beta-base .bodyContainer td.rightSidebar p {
-			margin: 0;
-			color: inherit;
-			}
-			.beta-base .bodyContainer td.header div.title, .beta-base .bodyContainer td.preheader div.title, .beta-base .bodyContainer td.body div.title,
-			.beta-base .bodyContainer td.footer div.title, .beta-base .bodyContainer td.sidebar div.title,
-			.beta-base .bodyContainer td.leftSidebar div.title, .beta-base .bodyContainer td.rightSidebar div.title,
-			.beta-base .bodyContainer td.header div.subtitle, .beta-base .bodyContainer td.preheader div.subtitle, .beta-base .bodyContainer td.body div.subtitle,
-			.beta-base .bodyContainer td.footer div.subtitle, .beta-base .bodyContainer td.sidebar div.subtitle,
-			.beta-base .bodyContainer td.leftSidebar div.subtitle, .beta-base .bodyContainer td.rightSidebar div.subtitle,
-			.beta-base .bodyContainer td.header div.text, .beta-base .bodyContainer td.preheader div.text, .beta-base .bodyContainer td.body div.text,
-			.beta-base .bodyContainer td.footer div.text, .beta-base .bodyContainer td.sidebar div.text,
-			.beta-base .bodyContainer td.leftSidebar div.text, .beta-base .bodyContainer td.rightSidebar div.text {
-			overflow: auto;
-			}
-			.beta-base .optout {
-			margin-bottom: 10px;
-			margin-top: 10px;
-			}
-			div.infusion-captcha {
-			width: 220px;
-			padding: 10px;
-			}
-			div.infusion-captcha input, div.infusion-captcha select, div.infusion-captcha textarea {
-			width: 95%;
-			display: inline-block;
-			vertical-align: middle;
-			}
-			table.infusion-field-container td.infusion-field-input-container input[type='text'],
-			table.infusion-field-container td.infusion-field-input-container input[type='password'],
-			table.infusion-field-container td.infusion-field-input-container textarea {
-			width: 98%; /* must be 98% to make the snippet-menu line up due to border width */
-			margin: 0;
-			}
-			table.infusion-field-container td.infusion-field-input-container select {
-			width: 101%;
-			*width: 102%; /* this one for IE */
-			margin: 0;
-			}
-			table.infusion-field-container td.infusion-field-label-container {
-			padding-right: 5px;
-			}
-			td.header .image-snippet img {
-			vertical-align: bottom;
-			}
-			#webformErrors {
-			color: #990000;
-			font-size: 14px;
-			}
-			html, body {
-			margin: 0;
-			padding: 0;
-			height: 100%;
-			}
-			.infusion-form {
-			margin: 0;
-			height: 100%;
-			}
-			.infusion-option {
-			display: block;
-			text-align: left;
-			}
-			</style>
-			<style type="text/css">
-			.beta-font-b h1, .beta-font-b h2, .beta-font-b h3, .beta-font-b h4, .beta-font-b h5, .beta-font-b h6 {
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b h1 {font-size: 24px;}
-			.beta-font-b h2 {font-size: 20px;}
-			.beta-font-b h3 {font-size: 14px;}
-			.beta-font-b h4 {font-size: 12px;}
-			.beta-font-b h5 {font-size: 10px;}
-			.beta-font-b h6 {font-size: 8px;}
-			.beta-font-b address {font-style: italic;}
-			.beta-font-b pre {font-family: Courier New, monospace;}
-			.beta-font-b .title, .beta-font-b .title p {
-			font-size: 20px;
-			font-weight: bold;
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b .subtitle, .beta-font-b .subtitle p {
-			font-size: 11px;
-			font-weight: normal;
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b .text, .beta-font-b p {
-			font-size: 12px;
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b .preheader .text, .beta-font-b .preheader .text p {
-			font-size: 11px;
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b .footer a {
-			font-size: 11px;
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b .footer .text {
-			font-size: 10px;
-			font-family: verdana,sans-serif;
-			}
-			.beta-font-b .sidebar .title, .beta-font-b .leftSidebar .title, .beta-font-b .rightSidebar .title {
-			font-size: 15px;
-			font-weight: bold;
-			font-family: arial,sans-serif;
-			}
-			.beta-font-b .sidebar .subtitle, .beta-font-b .leftSidebar .subtitle, .beta-font-b .rightSidebar .subtitle {
-			font-size: 12px;
-			font-family: arial, sans-serif;
-			}
-			.beta-font-b .sidebar .text, .beta-font-b .leftSidebar .text, .beta-font-b .rightSidebar .text {
-			font-size: 11px;
-			font-family: arial, sans-serif;
-			}
-			.infusion-field-label-container {
-			font-size: 14px;
-			font-family: arial,sans-serif;
-			}
-			.infusion-field-input-container {
-			color: #000000;
-			font-size: 12px;
-			}
-			.infusion-option label {
-			color: #000000;
-			font-size: 14px;
-			font-family: arial,sans-serif;
-			}
-			</style>
-			<style type="text/css">
-			/* order is very important!  Overridden classes MUST come first */
-			.default a {
-			color: #F15C25;
-			}
-			.default .text {
-			color: #787878;
-			}
-			.default .preheader a {
-			color: #2A2928;
-			}
-			.default .preheader .text {
-			color: #2A2928;
-			}
-			.default .header {
-			background-color: #FFFFFF;
-			}
-			.default .title {
-			color: #669940;
-			}
-			.default .subtitle {
-			color: #F15C25;
-			}
-			.default .sidebar .title, .default .leftSidebar .title, .default .rightSidebar .title {
-			color: #F15C25;
-			}
-			.default .sidebar .subtitle, .default .leftSidebar .subtitle, .default .rightSidebar .subtitle {
-			color: #669940;
-			}
-			.default .footer {
-			background-color: #7AC143;
-			}
-			.default .footer a {
-			color: #00728F;
-			}
-			.default .footer .text {
-			color: #2A2928;
-			}
-			.default .infusion-field-label-container {
-			color: #000000;
-			font-size: 14px;
-			font-family: arial,sans-serif;
-			}
-			.default .infusion-field-input-container {
-			color: #000000;
-			font-size: 12px;
-			}
-			.default .infusion-option label {
-			font-size: 14px;
-			color: #000000;
-			font-family: arial,sans-serif;
-			}
-			</style>
-			<style type="text/css">
-			.infusion-field-label-container {
-			text-align:Left;
-			}
-			.infusion-field-label-container {
-			vertical-align:Middle;
-			}
-			.infusion-field-input-container {
-			width:200px;
-			}
-			.bodyContainer {
-			width:425px;
-			}
-			</style>
+        <h1>Coletivos & Artistas</h1>
+        <p>Se você é um artista ou representa algum coletivo, e gostaria de fazer parte ou descobrir como o IMAGINA RIO ajuda a divulgar você, preencha o formulário abaixo, e nós entraremos em contato com você diretamente.</p>
 			<script src="https://ky108.infusionsoft.com/app/webTracking/getTrackingCode?trackingId=4f04c36b6b1db8ad14a88bb9450b4899" type="text/javascript">
 			</script>
+			<div class="formContainer">
 			<form accept-charset="UTF-8" action="https://ky108.infusionsoft.com/app/form/process/bc82c6b497f81af8960c2e3ddb61e95f" class="infusion-form" method="POST" name="Artista/Coletivo" onsubmit="var form = document.forms[0];
 			var resolution = document.createElement('input');
 			resolution.setAttribute('id', 'screenResolution');
@@ -338,7 +113,7 @@
 			</div>
 			<div>
 			<label for="inf_custom_Descricao">Descri&ccedil;&atilde;o *</label>
-			<input class="infusion-field-input-container" id="inf_custom_Descricao" name="inf_custom_Descricao" type="text" />
+			<input class="infusion-field-input-container contDescr" id="inf_custom_Descricao" name="inf_custom_Descricao" type="text" />
 			</div>
 			<div>
 			<div class="infusion-captcha" style="-moz-border-radius:10px; background-color:#CCCCCC; border-radius:10px; border: 1px solid #000000; color:#000000; margin-left:auto; margin-right:auto">
@@ -363,10 +138,11 @@
 			</div>
 			<div>
 			<div class="infusion-submit" style="text-align:center">
-			<button style="" type="submit" value="Enviar">Enviar</button>
+			<button style="" class="btn btn-primary" type="submit" value="Enviar">Enviar</button>
 			</div>
 			</div>
 			</form>
+			</div>
      
         </div>
 
--- fuel/app/views/web/forms/individual.php
+++ fuel/app/views/web/forms/individual.php
@@ -7,15 +7,102 @@
     <?php echo Asset::css('main.css'); ?>
 
 </head>
-<body class="zulub">
+<body class="individual">
     <div id="vignette" class="container-fluid">     
 
         <span id="logo" class="offset2"></span>
 
         <div class="texto-wrapper">
-        <h1>Quem somos</h1>
-
-        
+        <h1>Público Geral</h1>
+        <p>Se você se interessou pelo IMAGINA RIO, tem alguma dúvida ou simplesmente gostaria de saber mais sobre o projeto e o que ele representa, preencha o formulário abaixo e nós entraremos em contato com você.</p>
+        <script src="https://ky108.infusionsoft.com/app/webTracking/getTrackingCode?trackingId=4f04c36b6b1db8ad14a88bb9450b4899" type="text/javascript">
+</script>
+<div class="formContainer">
+<form accept-charset="UTF-8" action="https://ky108.infusionsoft.com/app/form/process/99cd47306a50770e8bf056d427569d69" class="infusion-form" method="POST" name="P&uacute;blico" onsubmit="var form = document.forms[0];
+var resolution = document.createElement('input');
+resolution.setAttribute('id', 'screenResolution');
+resolution.setAttribute('type', 'hidden');
+resolution.setAttribute('name', 'screenResolution');
+var resolutionString = screen.width + 'x' + screen.height;
+resolution.setAttribute('value', resolutionString);
+form.appendChild(resolution);
+var pluginString = '';
+if (window.ActiveXObject) {
+    var activeXNames = {'AcroPDF.PDF':'Adobe Reader',
+        'ShockwaveFlash.ShockwaveFlash':'Flash',
+        'QuickTime.QuickTime':'Quick Time',
+        'SWCtl':'Shockwave',
+        'WMPLayer.OCX':'Windows Media Player',
+        'AgControl.AgControl':'Silverlight'};
+    var plugin = null;
+    for (var activeKey in activeXNames) {
+        try {
+            plugin = null;
+            plugin = new ActiveXObject(activeKey);
+        } catch (e) {
+            // do nothing, the plugin is not installed
+        }
+        pluginString += activeXNames[activeKey] + ',';
+    }
+    var realPlayerNames = ['rmockx.RealPlayer G2 Control',
+        'rmocx.RealPlayer G2 Control.1',
+        'RealPlayer.RealPlayer(tm) ActiveX Control (32-bit)',
+        'RealVideo.RealVideo(tm) ActiveX Control (32-bit)',
+        'RealPlayer'];
+    for (var index = 0; index &lt; realPlayerNames.length; index++) {
+        try {
+            plugin = new ActiveXObject(realPlayerNames[index]);
+        } catch (e) {
+            continue;
+        }
+        if (plugin) {
+            break;
+        }
+    }
+    if (plugin) {
+        pluginString += 'RealPlayer,';
+    }
+} else {
+    for (var i = 0; i &lt; navigator.plugins.length; i++) {
+        pluginString += navigator.plugins[i].name + ',';
+    }
+}
+pluginString = pluginString.substring(0, pluginString.lastIndexOf(','));
+var plugins = document.createElement('input');
+plugins.setAttribute('id', 'pluginList');
+plugins.setAttribute('type', 'hidden');
+plugins.setAttribute('name', 'pluginList');
+plugins.setAttribute('value', pluginString);
+form.appendChild(plugins);
+var java = navigator.javaEnabled();
+var javaEnabled = document.createElement('input');
+javaEnabled.setAttribute('id', 'javaEnabled');
+javaEnabled.setAttribute('type', 'hidden');
+javaEnabled.setAttribute('name', 'javaEnabled');
+javaEnabled.setAttribute('value', java);
+form.appendChild(javaEnabled);">
+<input name="inf_form_xid" type="hidden" value="99cd47306a50770e8bf056d427569d69" /><input name="inf_form_name" type="hidden" value="P&uacute;blico" /><input name="infusionsoft_version" type="hidden" value="1.28.7.17" />
+<div class="default beta-base beta-font-b" id="mainContent" style="height:100%">
+<div class="text" id="webformErrors" name="errorContent">
+</div>
+<div>
+<label for="inf_field_FirstName">Nome *</label>
+<input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="text" />
+<label for="inf_field_LastName">Sobrenome *</label>
+<input class="infusion-field-input-container" id="inf_field_LastName" name="inf_field_LastName" type="text" />
+</div>
+<div>
+<label for="inf_field_Email">Email *</label>
+<input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="text" />
+</div>
+<div>
+<div class="infusion-submit" style="text-align:center">
+<button style="" class="btn btn-primary" type="submit" value="Enviar">Enviar</button>
+</div>
+</div>
+</div>
+</form>
+</div>
         </div>
 
         <?php echo render('modules/menu'); ?>
