925f4024f6671fc1882716256e122f468a778229
Christian Bueno <me@christianbueno.com>
2013-04-10 13:13:01 -0300

    Ajustes página interna de coletivo
    Signed-off-by: Christian Bueno <me@christianbueno.com>

--- fuel/app/views/web/coletivos/pagina.php
+++ fuel/app/views/web/coletivos/pagina.php
@@ -17,7 +17,7 @@
         
         <div class="texto-wrapper">
 
-        <img src="/assets/img/headerCole.png" alt="headerCole" width="62" height="63" style="margin-bottom:21px;float:left;"/><h1 style="width:640px;float:left;"><?php echo $coletivo->name ?></h1>
+        <img src="/assets/img/headerCole.png" alt="headerCole" width="62" height="63" style="margin:16px 0px 0px 16px;float:left;"/><h1 style="width:640px;float:left;"><?php echo $coletivo->name ?></h1>
         <div class="coleMap" id="map-canvas"></div>
         <h2>Sobre o Coletivo</h2>
         <p><?php echo $coletivo->description ?></p>
