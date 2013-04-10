dbd921e6ffc2afbb3256b1d3614ed3b2c7b20da4
Christian Bueno <me@christianbueno.com>
2013-04-09 23:52:00 -0300

    Ajustes Gerais
    -Correção dos fundos das páginas para que elas cubram toda a tela.
    -Criação da página 'Fale Conosco'.
    -Criação de botões na página 'Fale Conosco'.
    
    Signed-off-by: Christian Bueno <me@christianbueno.com>

--- fuel/app/views/web/forms/faleconosco.php
+++ fuel/app/views/web/forms/faleconosco.php
@@ -7,15 +7,29 @@
     <?php echo Asset::css('main.css'); ?>
 
 </head>
-<body class="zulub">
+<body class="fale-conosco">
     <div id="vignette" class="container-fluid">     
 
-        <span id="logo" class="offset2"></span>
+        <span id="logo" class="offset2 branco"></span>
 
         <div class="texto-wrapper">
         <h1>Fale conosco</h1>
-
         
+        <p>A IMAGINA RIO é feita de você, da sua participação e do seu envolvimento, e é por isso que a IMAGINA RIO busca meios de você fazer parte do nosso grande mesclado, seja você o representante de um <strong>coletivo</strong> ou <strong>escola</strong>, um <strong>artista</strong>, ou simplesmente uma pessoa interessada em cultura e integração.</p>
+	        <div class="row">
+		        <div class="span3 fcBlock">
+		        	<img src="/assets/img/coletivosfc.png" alt="coletivosfc" width="180" height="180">
+			        <span class="fcButton">Coletivos/Artistas</span>
+		        </div>
+		        <div class="span3 fcBlock">
+		        	<img src="/assets/img/escolasfc.png" alt="escolasfc" width="180" height="180">
+		        	<span class="fcButton">Escolas</span>		        
+		        </div>
+		        <div class="span3 fcBlock">
+		        	<img src="/assets/img/individuofc.png" alt="individuofc" width="180" height="180">
+			        <span class="fcButton">Indivíduos</span>
+		        </div>
+	        </div>
         </div>
 
         <?php echo render('modules/menu'); ?>
