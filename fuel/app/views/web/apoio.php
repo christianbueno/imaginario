20a00c33cffa7bc5a4890a8f1d063b185419024a
Christian Bueno <me@christianbueno.com>
2013-04-10 10:15:53 -0300

    Ajustes Página de Apoio
    Signed-off-by: Christian Bueno <me@christianbueno.com>

--- fuel/app/views/web/apoio.php
+++ fuel/app/views/web/apoio.php
@@ -14,6 +14,45 @@
 
         <div class="texto-wrapper">
         <h1>Apoio</h1>
+        <p>A IMAGINA RIO só funciona graças à ajuda de nossos diversos colaboradores, se você também quer fazer parte desta família, entre em contato conosco.</p>
+        <div class="Patro1">
+        <span>
+        <img src="/assets/img/globo-rio.png" alt="Globo Rio" width="140" height="140">
+        </span>
+        <span>
+        <img src="/assets/img/prefeitura-rio.png" alt="Prefeitura do Rio" width="140" height="140">
+        </span>
+        <span>
+        <img src="/assets/img/riotur.png" alt="Riotur" width="140" height="140">
+        </span>       
+        <span>
+        <img src="/assets/img/metrorio.png" alt="Metrô Rio" width="140" height="140">
+        </span>           
+        <span>
+        <img src="/assets/img/cuca.png" alt="CUCA" width="140" height="140">
+        </span> 
+        <span>
+        <img src="/assets/img/iorbita.png" alt="iOrbita" width="140" height="140">
+        </span>
+        <span>
+        <img src="/assets/img/papirus.png" alt="Papirus" width="140" height="140">
+        </span>
+        <span>
+        <img src="/assets/img/abcurbana.png" alt="ABC Urbana" width="140" height="140">
+        </span>
+        <span>
+        <img src="/assets/img/congraf.png" alt="Congraf" width="140" height="140">
+        </span>
+        <span>
+        <img src="/assets/img/sunchemical.png" alt="Sun Chemical" width="140" height="140">
+        </span>                                                                     
+<!--
+        <div>
+        <h2 style="margin-top:30px;">Globo Rio</h1>
+        <p>A Globo Rio sempre esteve engajada com a cultura na Cidade Maravilhosa, e com o IMAGINA RIO não podia ser diferente.</p>
+        </div>
+-->
+        </div>
         
 
         
