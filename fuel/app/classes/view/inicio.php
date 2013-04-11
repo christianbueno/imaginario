1425e8c49db8094a5255bdaa24b1f515caa9297a
THIAGO CRUZ <othiagocruz@outlook.com>
2013-04-11 19:31:31 -0300

    bug na home

--- fuel/app/classes/view/inicio.php
+++ fuel/app/classes/view/inicio.php
@@ -12,10 +12,8 @@ class View_Inicio extends ViewModel
             )
         ));
         foreach ($images as $image) {
-            $image->info = unserialize($image['metadata']);
-            Debug::dump($image);
-            $coletivo = Model_Coletivo::find($image->coletivo_id);
-            Debug::dump($coletivo->metadata, $image->coletivo_id);
+            $image->info = unserialize($image['metadata']);            
+            $coletivo = Model_Coletivo::find($image->coletivo_id);            
             $coletivo->info = unserialize($coletivo->metadata);
             $image->coletivo = $coletivo;
         }                
