a2c209608721e672485a3a341314e5d6708bb20a
THIAGO CRUZ <othiagocruz@outlook.com>
2013-04-11 20:18:40 -0300

    bug na home

--- fuel/app/views/web/inicio.php
+++ fuel/app/views/web/inicio.php
@@ -21,23 +21,20 @@
 				<img src="assets/img/twitter_32.png" alt="twitter_32" width="32" height="32" />
 				<img src="assets/img/vimeo_32.png" alt="vimeo_32" width="32" height="32" />
 			</div>	
-			<?php 
-			$slider1 = array_slice($images, 0, 20, true);
-			$siider2 = array_slice($images, 21, 41, true);
-			?>
+
 			<div id="kaled" class="row">
 				<div id="holder">
 					<div class="slider">
-						<?php $renderSlider($slider1); ?>
+						<?php $renderSlider(array_slice($images, 0, 20)); ?>
 					</div>
 					<div class="slider">
-						<?php $renderSlider($slider2); ?>
+						<?php $renderSlider(array_slice($images, 21)); ?>
 					</div>
 					<div class="slider">
-						<?php $renderSlider($slider1); ?>
+						<?php $renderSlider(array_slice($images, 0, 20)); ?>
 					</div>
 					<div class="slider">
-						<?php $renderSlider($slider1); ?>
+						<?php $renderSlider(array_slice($images, 21)); ?>
 					</div>				
 				</div>
 			</div>
