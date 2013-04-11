6cd5b3b7b72dc8d6c69a3d405855462f82b830ff
THIAGO CRUZ <othiagocruz@outlook.com>
2013-04-11 19:59:28 -0300

    bug na home

--- fuel/app/views/web/inicio.php
+++ fuel/app/views/web/inicio.php
@@ -27,13 +27,13 @@
 						<?php $renderSlider(array_slice($images, 0, 20)); ?>
 					</div>
 					<div class="slider">
-						<?php $renderSlider(array_slice($images, 21, 41)); ?>
+						<?php $renderSlider(array_slice($images, 21, 40)); ?>
 					</div>
 					<div class="slider">
 						<?php $renderSlider(array_slice($images, 0, 20)); ?>
 					</div>
 					<div class="slider">
-						<?php $renderSlider(array_slice($images, 21, 41)); ?>
+						<?php $renderSlider(array_slice($images, 21, 40)); ?>
 					</div>				
 				</div>
 			</div>
