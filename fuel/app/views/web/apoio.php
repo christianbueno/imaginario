d7fb9ab147e64830ae4bc1bd7d16c6f2c1ac2253
THIAGO CRUZ <othiagocruz@outlook.com>
2013-04-11 20:46:50 -0300

    favicon

--- fuel/app/views/web/apoio.php
+++ fuel/app/views/web/apoio.php
@@ -18,34 +18,34 @@
         <p>Se você também quer fazer parte desta família, entre em contato conosco.</p>
         <div class="Patro1">
         <span>
-        <a href="http://redeglobo.globo.com/" target="_blank"><img src="/assets/img/globo-rio.png" alt="Globo Rio" width="140" height="140">
+        <a href="http://redeglobo.globo.com/" target="_blank"><img src="/assets/img/globo-rio.png" alt="Globo Rio" width="140" height="140"></a>
         </span>
         <span>
         <a href="http://www.rio.rj.gov.br/" target="_blank"><img src="/assets/img/prefeitura-rio.png" alt="Prefeitura do Rio" width="140" height="140"></a>
         </span>
         <span>
-        <a href="http://www.rioguiaoficial.com.br/" target="_blank"><img src="/assets/img/riotur.png" alt="Riotur" width="140" height="140">
+        <a href="http://www.rioguiaoficial.com.br/" target="_blank"><img src="/assets/img/riotur.png" alt="Riotur" width="140" height="140"></a>
         </span>       
         <span>
-        <a href="http://www.metrorio.com.br/" target="_blank"><img src="/assets/img/metrorio.png" alt="Metrô Rio" width="140" height="140">
+        <a href="http://www.metrorio.com.br/" target="_blank"><img src="/assets/img/metrorio.png" alt="Metrô Rio" width="140" height="140"></a>
         </span>           
         <span>
-        <a href="http://www.cucadesign.com/" target="_blank"><img src="/assets/img/cuca.png" alt="CUCA" width="140" height="140">
+        <a href="http://www.cucadesign.com/" target="_blank"><img src="/assets/img/cuca.png" alt="CUCA" width="140" height="140"></a>
         </span> 
         <span>
-        <a href="http://www.facebook.com/IOrbitaProd" target="_blank"><img src="/assets/img/iorbita.png" alt="iOrbita" width="140" height="140">
+        <a href="http://www.facebook.com/IOrbitaProd" target="_blank"><img src="/assets/img/iorbita.png" alt="iOrbita" width="140" height="140"></a>
         </span>
         <span>
-        <a href="http://www.papirus.com/" target="_blank"><img src="/assets/img/papirus.png" alt="Papirus" width="140" height="140">
+        <a href="http://www.papirus.com/" target="_blank"><img src="/assets/img/papirus.png" alt="Papirus" width="140" height="140"></a>
         </span>
         <span>
-        <a href="http://www.abcurbana.com.br/" target="_blank"><img src="/assets/img/abcurbana.png" alt="ABC Urbana" width="140" height="140">
+        <a href="http://www.abcurbana.com.br/" target="_blank"><img src="/assets/img/abcurbana.png" alt="ABC Urbana" width="140" height="140"></a>
         </span>
         <span>
-        <a href="http://www.congraf.com.br/" target="_blank"><img src="/assets/img/congraf.png" alt="Congraf" width="140" height="140">
+        <a href="http://www.congraf.com.br/" target="_blank"><img src="/assets/img/congraf.png" alt="Congraf" width="140" height="140"></a>
         </span>
         <span>
-        <a href="http://www.sunchemical.com/" target="_blank"><img src="/assets/img/sunchemical.png" alt="Sun Chemical" width="140" height="140">
+        <a href="http://www.sunchemical.com/" target="_blank"><img src="/assets/img/sunchemical.png" alt="Sun Chemical" width="140" height="140"></a>
         </span>                                                                     
 <!--
         <div>
