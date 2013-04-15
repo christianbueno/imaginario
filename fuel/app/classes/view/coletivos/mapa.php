b788c4b396f041f13562a45e1555bd9c319a3288
THIAGO CRUZ <othiagocruz@outlook.com>
2013-04-15 16:07:26 -0300

    correcoes

--- fuel/app/classes/view/coletivos/mapa.php
+++ fuel/app/classes/view/coletivos/mapa.php
@@ -10,10 +10,11 @@ class View_Coletivos_Mapa extends ViewModel
 
         foreach ($coletivos as $coletivo) {
             $coletivo->info = unserialize($coletivo->metadata);
+            $coletivo->logo = isset($coletivo->info['logo']) ? $coletivo->info['logo'] : '';
+            $coletivo->cor = isset($coletivo->info['cor']) ? $coletivo->info['cor'] : '';
+            $coletivo->latlng = isset($coletivo->info['latlng']) ? $coletivo->info['latlng'] : '';
         }
-        $coletivo->logo = isset($coletivo->info['logo']) ? $coletivo->info['logo'] : '';
-        $coletivo->cor = isset($coletivo->info['cor']) ? $coletivo->info['cor'] : '';
-        $coletivo->latlng = isset($coletivo->info['latlng']) ? $coletivo->info['latlng'] : '';
+
 
         $this->coletivos = $coletivos;
     }
