e20ab1575ab1ec1a8b82c4293079cc5095f5d7a8
THIAGO CRUZ <othiagocruz@outlook.com>
2013-04-28 22:21:50 -0300

    navegacao do menu do admin

--- fuel/app/views/admin/menu.php
+++ fuel/app/views/admin/menu.php
@@ -3,4 +3,5 @@
     <?php echo Html::anchor('admin/usuario', 'Usuários', array('class' => 'btn btn-default')); ?>   
     <?php echo Html::anchor('admin/conteudo', 'Conteudo', array('class' => 'btn btn-default')); ?>
     <?php echo Html::anchor('admin/evento', 'Eventos', array('class' => 'btn btn-default')); ?>      
+    <?php echo Html::anchor('admin/config', 'Configurações', array('class' => 'btn btn-default')); ?>    
 </nav> 
