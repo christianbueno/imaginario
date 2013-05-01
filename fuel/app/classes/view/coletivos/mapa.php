f667a148eac85dbd03e4d88101fdae9e514ee313
THIAGO CRUZ <othiagocruz@outlook.com>
2013-05-01 05:31:09 -0300

    box de coletivos

--- fuel/app/classes/view/coletivos/mapa.php
+++ fuel/app/classes/view/coletivos/mapa.php
@@ -16,6 +16,7 @@ class View_Coletivos_Mapa extends ViewModel
             ));
 
             $coletivo->cor = isset($coletivo->info['cor']) ? $coletivo->info['cor'] : 'FF7200';
+            $coletivo->thumb = isset($thumb) ? "/arquivos/thumb-$thumb->content" : '/assets/img/thumb-noicon.jpg';
             $coletivo->background = isset($thumb) ? "url(/arquivos/thumb-$thumb->content) -50px -50px" : "url(/assets/img/thumb-noicon.jpg) -50px -50px";
             $coletivo->ref = isset($coletivo->info['logo']) ? $coletivo->info['logo'] : '';
             
--- fuel/app/views/web/coletivos/mapa.php
+++ fuel/app/views/web/coletivos/mapa.php
@@ -6,8 +6,10 @@
     <?php echo Asset::css('bootstrap.css'); ?>
     <?php echo Asset::css('main.css'); ?>
     <?php echo Asset::js('analytics.js'); ?>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSUuzKFf-qvUvfah7ekcUoJPB3V1DUDmw&sensor=false"></script>
-  <script type="text/javascript" src="/assets/js/markerwithlabel.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSUuzKFf-qvUvfah7ekcUoJPB3V1DUDmw&sensor=false"></script>  
+    <!--script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js"></script-->
+    <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/richmarker/src/richmarker-compiled.js"></script>
+    
   
   
 <style type="text/css">
@@ -24,6 +26,9 @@
     display:table;
     line-height:120px;
     border-radius: 60px; 
+    position: relative;
+    bottom: 66px;
+    left: 11px;
     -moz-border-radius: 60px; 
     -webkit-border-radius: 60px;
 }
@@ -38,10 +43,22 @@
 </style> 
 </head>
 <body class="noBG">
+    <div id="box-coletivos">
+        <h1>Coletivos</h1>
+        <ul id="box-coletivos-lista">
+        <?php foreach ($coletivos as $coletivo):
+            $coletivo_id = $coletivo->id;
+            $coletivo_name = Inflector::friendly_title($coletivo->name, '-', true);                
+            $url = "/coletivos/ver/$coletivo_name/$coletivo_id";
+        ?>   
+
+            <li><?php echo Html::anchor($url, Html::img($coletivo->thumb, array('class' => 'i-thumb')) . $coletivo->name, array('style' => "background-color: #$coletivo->cor")); ?></li>
+        <?php endforeach; ?>
+    </div>
     <div class="bgMap" id="map-canvas"></div>
     <div id="vignette" class="container-fluid">     
 
-        <a href="http://imaginario.etc.br" id="logo" class="offset2"></a>        
+        <a href="/" id="logo" class="offset2"></a>        
 
         
 
@@ -54,12 +71,18 @@
         <?php echo render('modules/menu'); ?>
     </div>
 <?php echo Asset::js('jquery-1.9.1.min.js'); ?>
+<?php echo Asset::js('jquery-ui-1.10.2.custom.min.js'); ?>
 <?php echo Asset::js('main.js'); 
 
 ?>
 
 <script>
 $(document).ready(function(){
+    $( "#box-coletivos" ).draggable({ 
+        handle: "h1",
+        scroll: false,
+        containment: 'parent'
+    });
     var mapOptions = {        
         zoom: 12,        
         zoomControl: true,
@@ -76,14 +99,17 @@ $(document).ready(function(){
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     
 
-<?php foreach ($coletivos as $coletivo):?>   
+<?php 
+    $i = 666;
+    foreach ($coletivos as $coletivo):
+?>   
 	
 
 
     var location = new google.maps.LatLng(<?php echo $coletivo->latlng; ?>);
     
 
-    var seta = {
+    var seta<?php echo $coletivo->id; ?> = {
         path: 'M 48.4 159 C 33.6 139.9 21.8 126.4 20.6 123.8 C 14.5 110.9 10.6 110 6.8 97 C 5.2 91.6 2.4 71 7.7 51.5 C 12.1 35.5 33 15.9 45.7 11 C 51.8 8.6 59 5.7 75.3 5.7 C 91.6 5.7 95.1 8.3 101.3 10.4 C 121.5 17.4 138.1 32.3 145.1 58.6 C 147.6 67.9 145.8 93.2 142.9 99 C 135.4 114.1 132.2 120.9 128.1 126.3 C 126.4 128.6 119.6 139.5 105 157.8 C 86.5 180.9 79.4 191.7 81.2 189.1 C 81.8 188.3 77.9 192.8 75 191.8 C 70 190.1 62 176.6 48.4 159Z M 135.6 61.9 C 47.7 149.5 91.7 105.7 135.6 61.9Z',
         fillColor: "#<?php echo $coletivo->cor; ?>",
         fillOpacity: 1,
@@ -95,26 +121,26 @@ $(document).ready(function(){
         url: '/assets/img/sombra.png',                
         anchor: new google.maps.Point(60, 200)
     };
-
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map,        
-        icon: seta,
+    <?php $i++; ?>
+    var marker<?php echo $coletivo->id; ?> = new google.maps.Marker({
+        position: location,        
+        map: map,       
+        zIndex: <?php echo $i ?>,     
+        optimized: false,  
+        flat: true,
+        icon: seta<?php echo $coletivo->id; ?>,        
         shadow: shadow        
     });
-	
-    var thumb = new MarkerWithLabel({
-        position: location,
-        map: map,
-		labelContent: "<?php echo $coletivo->name; ?>",
-	    labelAnchor: new google.maps.Point(50, 190),
-		labelClass: "foto fundo<?php echo $coletivo->id; ?>",
-		labelVisible: true,	
-		shadow:shadow,	
-        icon: {path:''},
-		labelInForeground:false
-    });   		   	
-   
+    <?php $i++; ?>
+    var thumb<?php echo $coletivo->id; ?> = new RichMarker({
+        position: location,        
+        map: map,  
+        shadow: null,
+        zIndex: <?php echo $i ?>,
+        flat: true,          
+        optimized: false,  
+        content: '<span class="fundo<?php echo $coletivo->id; ?> foto"><?php echo $coletivo->name; ?></span>',
+    });
 
     <?php 
         $coletivo_id = $coletivo->id;
@@ -122,21 +148,25 @@ $(document).ready(function(){
         $url = "/coletivos/ver/$coletivo_name/$coletivo_id";
     ?>
 
-    google.maps.event.addListener(thumb, 'click', function(e) {
+    google.maps.event.addListener(marker<?php echo $coletivo->id; ?>, 'click', function(e) {
         window.location.href = '<?php echo $url; ?>';
     });
-    google.maps.event.addListener(thumb, 'mouseover', function(e) {
+    google.maps.event.addListener(marker<?php echo $coletivo->id; ?>, 'mouseover', function(e) {
         $current = $(e.target);
 
         $current.addClass('hovered');
     });
-    google.maps.event.addListener(thumb, 'mouseout', function(e) {
+    google.maps.event.addListener(marker<?php echo $coletivo->id; ?>, 'mouseout', function(e) {
         $current = $(e.target);
 
         $current.removeClass('hovered');
     });    
+
+    
 <?php endforeach; ?> 
 });
 
 </script>
+
+
 </body></html>
