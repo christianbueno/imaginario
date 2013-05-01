35df4442f915d5a928bc2cb635076736fa96e3b8
Christian Bueno <me@christianbueno.com>
2013-05-01 01:21:05 -0400

    Correção do problema do footer
    Signed-off-by: Christian Bueno <me@christianbueno.com>

--- fuel/app/views/web/forms/faleconosco.php
+++ fuel/app/views/web/forms/faleconosco.php
@@ -39,13 +39,13 @@
         </div>
 
         <?php echo render('modules/menu'); ?>
-    <footer class="copyFooter">
-    	<a href="/copyright">Copyright</a> | <a href="/politicas-de-privacidade">Política de Privacidade</a> | <a href="/anti-spam">Anti-Spam</a><br/>
-    	&copy; Imagina: Rio 2013 - Todos os direitos reservados. | Rua Humaitá, 58 - Casa 2 - Humaitá<br/>
-    	Rio de Janeiro, RJ Brasil 22261-001 | <a href="mailto:contato@imaginario.etc.br">contato@imaginario.etc.br</a><br/>
-    	<a href="http://www.babelteam.com/pt">Babel-Team – Marketing & Vendas Automatizadas</a>
-    </footer>
     </div>
+    <footer class="copyFooter">
+        <a href="/copyright">Copyright</a> | <a href="/politicas-de-privacidade">Política de Privacidade</a> | <a href="/anti-spam">Anti-Spam</a><br/>
+        &copy; Imagina: Rio 2013 - Todos os direitos reservados. | Rua Humaitá, 58 - Casa 2 - Humaitá<br/>
+        Rio de Janeiro, RJ Brasil 22261-001 | <a href="mailto:contato@imaginario.etc.br">contato@imaginario.etc.br</a><br/>
+        <a href="http://www.babelteam.com/pt">Babel-Team – Marketing & Vendas Automatizadas</a>
+    </footer>
 <?php echo Asset::js('jquery-1.8.3.min.js'); ?>
 <?php echo Asset::js('main.js'); ?>
 </body></html>
