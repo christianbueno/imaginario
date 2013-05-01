f28c95b0a417ebecaf998666f95863e9a7dcade9
Christian Bueno <me@christianbueno.com>
2013-04-30 22:32:25 -0400

    Páginas de Copyright, Politica de Privacidade e Anti-Spam
    Signed-off-by: Christian Bueno <me@christianbueno.com>

--- fuel/app/views/web/politicas.php
+++ fuel/app/views/web/politicas.php
@@ -2,7 +2,7 @@
 <html><head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
-    <title>Imagina.RIO > Agradecimentos > Politicas</title>
+    <title>Imagina.RIO > Política de Privacidade</title>
     <?php echo Asset::css('bootstrap.css'); ?>
     <?php echo Asset::css('main.css'); ?>
     <?php echo Asset::js('analytics.js'); ?>
@@ -22,8 +22,23 @@
 
         <a href="http://imaginario.etc.br" id="logo" class="offset2"></a>
 
-        <div id="galeria" class="container">
-            politicas
+        <div class="texto-wrapper">
+            <h1>Política de Privacidade</h1>
+            <p>Leia atentamente a Política de Privacidade praticada pelo Imagina: Rio e os sites relacionados com a marca Imagina: Rio.</p>
+            <p>Ela tem por objetivo esclarecer a você como garantimos a proteção dos seus dados de usuário, fornecidos no preenchimento do cadastro em nossos sites. Caso tenha alguma dúvida, entre em contato pelo e-mail <a href="mailto:contato@imaginario.etc.br">contato@imaginario.etc.br</a> e teremos prazer em esclarecê-la.</p>
+            <p>As regras de nossa Política de Privacidade se aplicam a todas as áreas do site do Imagina: Rio e dos seus sites relacionados, mas não se aplicam a sites que por ventura estejam vinculados via links aos nossos nem às redes sociais. Você deve consultar a política de privacidade de cada site ou rede, para sua segurança.</p>
+            <p>Tráfego de dados e confidencialidade das informações de Usuário</p>
+            <p>Para ter acesso aos serviços do Imagina: Rio, você precisa antes preencher um cadastro no site, com suas informações pessoais. Este cadastro fica armazenado em nosso servidor, de maneira segura e confidencial, com acesso restrito a alguns integrantes da equipe do Imagina: Rio.</p>
+            <p>Garantimos que em nenhuma hipótese estas informações são cedidas ou vendidas para outras empresas, parceiras ou não do Imagina: Rio.</p>
+            <p>Também não tornamos públicas suas informações no site ou por qualquer outro meio.</p>
+            <p>Garantimos que seu e-mail não é compartilhado com terceiros, em nenhuma hipótese.</p>
+            <p>Garantimos que você só receberá mensagens de e-mail relativas à temática da página onde você se cadastrou, às atualizações, novidades da área, informativos eletrônicos do Imagina: Rio e informações sobre os coletivos, as datas e horários sobre as apresentações artísticas.</p>
+            <p>Garantimos que não fornecemos informações sobre sites hospedados em nossos servidores.</p>
+            <p>Só revelamos dados de nossos clientes em caso de ordem, pedido ou determinação judicial, caso em que tentaremos avisar você antes.</p>
+            <p>Garantimos que adotamos as melhores práticas e técnicas mais atuais de segurança e criptografia para impedir ou reduzir a ação criminosa na rede, protegendo aos extremos seus dados da possível ação de hackers.</p>
+            <p>Garantimos que quando você estiver navegando por nossas páginas somente coletamos informação estatística genérica, que nos ajudam a entender quais as páginas mais visitadas e o tempo de visita em cada uma, por exemplo, para desenvolver um serviço cada vez melhor para você.</p>
+            <p>Garantir sua privacidade é mais do que um impositivo legal, é nosso princípio.</p>
+            <p>Para perguntas sobre nossa Política de Privacidade, envie um e-mail para <a href="mailto:contato@imaginario.etc.br">contato@imaginario.etc.br.</a></p>
         </div>
 
         <?php echo render('modules/menu'); ?>
--- fuel/app/views/web/spam.php
+++ fuel/app/views/web/spam.php
@@ -22,8 +22,16 @@
 
         <a href="http://imaginario.etc.br" id="logo" class="offset2"></a>
 
-        <div id="galeria" class="container">
-            spam
+        <div class="texto-wrapper">
+            <h1>Anti-Spam</h1>
+                <p>Esta declaração Anti-Spam rege o uso desta página web e seus serviços associados, outras páginas de Imagina: Rio, domínios e sub-domínios, que são de propriedade do Imagina: Rio. sob a marca Imagina: Rio.</p>
+<p>Onde quer que este aviso faça referência aos “usuários” isto significa “você”, enquanto “nós” ou “nosso” fazem referência ao Imagina: Rio e “Website” ao domínio <a href="http://imaginario.etc.br">www.imaginario.etc.br</a> e outros domínios de propriedade do Imagina: Rio sob a marca Imagina: Rio. Isto apresenta nossa política em relação ao uso de “Spam” como técnica de marketing ligada ao Marketing na Internet.</p>
+<p>Nós não enviamos emails em massa não solicitados ou emails não solicitados de qualquer espécie ligados ao marketing de nossos programas, produtos e serviços.</p>
+<p>Nós consideramos spam qualquer atividade na qual uma pessoa ou entidade transmita direta ou indiretamente mensagens por email à qualquer endereço de email que não solicitou tal mensagem e não autorizou esta transmissão.</p>
+<p>Nós também consideramos spam a postagem de anúncios em grupos de notícias que violem os termos de participação de tal grupo de notícias e que não tem relação com os assuntos, ou em grupos de notícias que especificamente não permitem anúncios.</p>
+<p>Nós também consideramos spam quando um anúncio é colocado em fóruns ou salas de chat quando estes não são permitidos pelos termos de participação em tais fóruns ou salas de chat. Se você for vítima de spam por qualquer pessoa ligada aos nossos produtos, serviços, Website, ou qualquer outro assunto, por favor, relate esta atividade por e-mail para contato@imaginario.etc.br.</p>
+<p>Para perguntas sobre nossa Polítca Anti-Spam, envie um e-mail para <a href="mailto:contato@imaginario.etc.br">contato@imaginario.etc.br</a>.</p>
+
         </div>
 
         <?php echo render('modules/menu'); ?>
