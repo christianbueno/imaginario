bb21d26f9e5bd17004afde5bcd4c14ec228b6695
THIAGO CRUZ <othiagocruz@outlook.com>
2013-05-01 05:49:58 -0300

    coletivos em ordem alfabetica

--- fuel/app/classes/view/coletivos/mapa.php
+++ fuel/app/classes/view/coletivos/mapa.php
@@ -24,7 +24,7 @@ class View_Coletivos_Mapa extends ViewModel
         }
 
 
-        $this->coletivos = Arr::sort($coletivos, 'coletivo.name');
+        $this->coletivos = Arr::sort($coletivos, 'coletivo.name','asc', SORT_NATURAL);
     }
 
 
